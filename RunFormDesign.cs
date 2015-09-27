9ab1c7439a8a620d694502a0eb84ea813f84c280
DD~! <devddstuff@gmail.com>
2015-09-27 12:33:42 -0400

    Typo

--- RunFormDesign.cs
+++ RunFormDesign.cs
@@ -8,67 +8,70 @@ namespace Runmono
     {
         private void Init()
         {
-            this.SuspendLayout();
-            
-            this.Text = "Run";
-            this.HelpButton = true;
-            this.ShowIcon = false;
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.Size = new System.Drawing.Size (360, 170);
-            this.StartPosition = FormStartPosition.Manual;
-            this.Location =
-                new System.Drawing.Point(10,
+            SuspendLayout();
+
+            Text = "Run";
+            HelpButton = true;
+            ShowIcon = false;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Size = new Size(360, 170);
+            StartPosition = FormStartPosition.Manual;
+            Location =
+                new Point(10,
                     Screen.PrimaryScreen.WorkingArea.Height -
-                    (this.Size.Height + 10));
+                    (Size.Height + 10));
 
-            this.txt = new TextBox();
-            this.txt.KeyDown += txt_KeyDown;
-            this.txt.Location = new System.Drawing.Point(50, 60);
-            this.txt.Size = new System.Drawing.Size(290, 20);
+            txt = new TextBox();
+            txt.KeyDown += txt_KeyDown;
+            txt.Location = new Point(50, 60);
+            txt.Size = new System.Drawing.Size(290, 20);
 
-            this.browse = new Button();
-            this.browse.Text = "Browse...";
-            this.browse.Location = new System.Drawing.Point(this.Width - (browse.Size.Width + 20),
-                this.Height - (browse.Size.Height + 40));
+            browse = new Button();
+            browse.Text = "Browse...";
+            browse.Location = new Point(Width - (browse.Size.Width + 20),
+                Height - (browse.Size.Height + 40));
 
-            this.can = new Button();
-            this.can.Text = "Cancel";
-            this.can.Location = new System.Drawing.Point(this.Width - (can.Size.Width + browse.Size.Width + 30),
-                this.Height - (can.Size.Height + 40));
-            this.can.Click += can_Click;
+            can = new Button();
+            can.Text = "Cancel";
+            can.Location = new Point(Width - (can.Size.Width + browse.Size.Width + 30),
+                Height - (can.Size.Height + 40));
+            can.Click += can_Click;
 
-            this.run = new Button ();
-            this.run.Text = "Run";
-            this.run.Location = new System.Drawing.Point(this.Width - (can.Size.Width + browse.Size.Width + run.Size.Width + 40),
-                this.Height - (can.Size.Height + 40));
-            this.run.Click += run_Click;
+            run = new Button ();
+            run.Text = "Run";
+            run.Location =
+                new Point(Width - (can.Size.Width + browse.Size.Width + run.Size.Width + 40),
+                Height - (can.Size.Height + 40));
+            run.Click += run_Click;
 
-            this.type = new Label();
-            this.type.Text = "Type the name of a program, folder, document, or Internet ressource, and Windows will open it for you.";
-            this.type.Location = new System.Drawing.Point(50, 15);
-            this.type.Size = new System.Drawing.Size(280, 40);
+            type = new Label();
+            type.Text =
+                "Type the name of a program, folder, document, or Internet resource, and Windows will open it for you.";
+            type.Location = new Point(50, 15);
+            type.Size = new System.Drawing.Size(280, 40);
 
-            this.open = new Label();
-            this.open.Text = "Open:";
-            this.open.Location = new System.Drawing.Point(8, 63);
-            this.open.Size = new System.Drawing.Size(150, 20);
+            open = new Label();
+            open.Text = "Open:";
+            open.Location = new Point(8, 63);
+            open.Size = new System.Drawing.Size(150, 20);
 
-            this.pic = new PictureBox();
-            this.pic.Location = new Point(10, 20);
-            this.pic.Size = new Size(32, 32);
-            this.pic.Image = Image.FromStream(
-                Assembly.GetExecutingAssembly().GetManifestResourceStream("Runmono.serveimage.png")
-           );
+            pic = new PictureBox();
+            pic.Location = new Point(10, 20);
+            pic.Size = new Size(32, 32);
+            pic.Image = Image.FromStream(
+                Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("Runmono.serveimage.png")
+            );
 
-            this.Controls.Add(txt);
-            this.Controls.Add(run);
-            this.Controls.Add(can);
-            this.Controls.Add(browse);
-            this.Controls.Add(type);
-            this.Controls.Add(open);
-            this.Controls.Add(pic);
-            
-            this.ResumeLayout(false);
+            Controls.Add(txt);
+            Controls.Add(run);
+            Controls.Add(can);
+            Controls.Add(browse);
+            Controls.Add(type);
+            Controls.Add(open);
+            Controls.Add(pic);
+
+            ResumeLayout(false);
         }
 
         private Button browse;
